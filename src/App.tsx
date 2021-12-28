f9a04eb35b4b2ea04e30ec36986d998b2fe7c4de
Akinori <aki10.working@gmail.com>
2021-12-28 23:27:32 +0900

    test commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import {Node} from 'react';
+
+import HomeScreen from './components/pages/HomeScreen';
+import ProfileScreen from './components/pages/ProfileScreen';
+import WelcomeScreen from './components/pages/WelcomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+const App: () => Node = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Welcome'}}
+        />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
